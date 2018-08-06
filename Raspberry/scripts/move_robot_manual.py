caa3a6a7a8ffa90863a2e9c0ad3be5088ea4ae5c
Arturo Toro <arturoantoniotr@gmail.com>
2018-08-06 13:53:52 -0400

    Update move_robot_manual.py

--- Raspberry/scripts/move_robot_manual.py
+++ Raspberry/scripts/move_robot_manual.py
@@ -2,6 +2,10 @@ import sys
 import select
 import tty
 import termios
+pi = pigpio.pi()
+# Servo Parallax PWM: 1280–1720
+SERVOr = 4  # Modificar PINES PARA GPIO
+SERVOl = 2
 
 def isData():
     return select.select([sys.stdin], [], [], 0.25) == ([sys.stdin], [], [])
@@ -16,13 +20,25 @@ try:
         i += 1
         if isData():
             c = sys.stdin.read(1)
-            if c == '\x1b':         # x1b is ESC
-
+            if c == 'p':         # x1b is ESC
+                pi.set_servo_pulsewidth(SERVOr, 0)
+                pi.set_servo_pulsewidth(SERVOl, 0)
                 break
+            if(c == "w"):
+                pi.set_servo_pulsewidth(SERVOr, 1280)
+                pi.set_servo_pulsewidth(SERVOl, 1720)
+            elif(c == "s"):
+                pi.set_servo_pulsewidth(SERVOr, 1720)
+                pi.set_servo_pulsewidth(SERVOl, 1280)
+            elif(c == "a"):
+                pi.set_servo_pulsewidth(SERVOr, 1720)
+                pi.set_servo_pulsewidth(SERVOl, 1720)
+            elif(c == "d"):
+                pi.set_servo_pulsewidth(SERVOr, 1280)
+                pi.set_servo_pulsewidth(SERVOl, 1280)
             print("KEY", c)
             continue
-        print("NO LEO")
-
-
+        pi.set_servo_pulsewidth(SERVOr, 0)
+        pi.set_servo_pulsewidth(SERVOl, 0) 
 finally:
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
