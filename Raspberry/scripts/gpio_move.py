5c157e1a61c05d19b4dde36b0ed57202c269e51b
Pi3 <pi@gmail.com>
2018-08-13 22:28:00 +0000

    Scripts de movimiento

--- Raspberry/scripts/gpio_move.py
+++ Raspberry/scripts/gpio_move.py
@@ -6,14 +6,15 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
 
-r = GPIO.PWM(17, 50)
-l = GPIO.PWM(27, 50)
+l = GPIO.PWM(17, 50)
+r = GPIO.PWM(27, 50)
 
-r.start(6.4)
-l.start(6.4)
+r.start(6.2)
+l.start(8)
 try:
     while True:
-        pass
+        dc = float(input())
+        l.ChangeDutyCycle(dc)
         """
         p.ChangeDutyCycle(7.5)  # turn towards 90 degree
         time.sleep(1) # sleep 1 second
