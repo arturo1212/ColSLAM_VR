68e9687c01f616a2ea417b27c4af5ca1216e319c
Arturo Toro <arturoantoniotr@gmail.com>
2018-08-27 13:09:45 -0400

    Create ClawControl.cs

---
+++ Unity/Scripts/ClawControl.cs
@@ -0,0 +1,25 @@
+using Newtonsoft.Json;
+using RosSharp.RosBridgeClient;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawControl : MonoBehaviour {
+    RosSocket rosSocket;
+    public MessageProvider MessageProvider;
+    public string robotIP = "ws://192.168.1.105:9090";  // IP del robot.
+    public string Topic = "claw";
+    public int publicationId;
+
+    void Start()
+    {
+        rosSocket = new RosSocket(robotIP);
+        publicationId = rosSocket.Advertise(Topic, "std_msgs/String");
+    }
+
+    void Update () {
+        StandardString msg = new StandardString();
+        msg.data = "lala";
+        rosSocket.Publish(publicationId, msg);
+    }
+}
