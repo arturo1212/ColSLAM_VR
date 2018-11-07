b6d0c2a5c61064067bee485f42d709ea9669abe0
David A. Hernandez V <davidhervil@hotmail.com>
2018-11-07 13:04:44 -0400

    Merge de Grid y state machine

---
+++ Unity/Scripts/Robots/Robot.cs
@@ -0,0 +1,39 @@
+﻿using RosSharp.RosBridgeClient;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot:MonoBehaviour {
+    // Conexión
+    public string Ip = "192.168.1.104";
+
+    // Rangos de velocidades
+    [HideInInspector]
+    public int pwmRForward = 1280;
+    [HideInInspector]
+    public int pwmLForward = 1720;
+    [HideInInspector]
+    public int pwmRBackward = 1720;
+    [HideInInspector]
+    public int pwmLBackward = 1280;
+
+    /* Conexion con nodos de ROS */
+    [HideInInspector]
+    public RosSocket rosSocket;
+    [HideInInspector]
+    public int movementPublisherId;
+
+    // Velocidad de cada rueda (de 0 a 100).
+    public float RVelocity = 50, LVelocity = 50, RTurnVelocity=50, LTurnVelocity=50;
+
+    // Use this for initialization
+    void Start () {
+        rosSocket = new RosSocket(Ip);
+        rosSocket.Advertise("movement", "std_msgs/String");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Unity/Scripts/UserInput/Movement.cs
@@ -0,0 +1,159 @@
+﻿using RosSharp.RosBridgeClient;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour {
+
+    public bool behaviourIsRunning = false, stopped = false;
+    public bool facing = false, goingToGoal = false, traceDone = false, pathObstructed = false;
+    public Vector3? clickedPoint = null;
+
+
+    // Variables PWM en rueda.
+    public float RVelocity, LVelocity;
+
+    private Robot robot;
+
+    private int pwmRForward = 1280;
+    private int pwmLForward = 1720;
+    private int pwmRBackward = 1720;
+    private int pwmLBackward = 1280;
+
+    public NavMeshPath explorePath; // Path del que surge la explroacion
+    public Vector3 proximatePoint = new Vector3(-1,-1,-1); // Punto mas proxio al robot en el path de exploracion
+
+
+    // Use this for initialization
+    void Start () {
+        robot = gameObject.GetComponent<Robot>();
+        explorePath = new NavMeshPath();
+    }
+
+    // Update is called once per frame
+
+    public void WASD()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            GoForward();
+            //print("W key was pressed");
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            TurnLeft();
+            //print("A key was pressed");
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            GoBackwards();
+            //print("S key was pressed");
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            TurnRight();
+            //print("D key was pressed");
+        }
+        else
+        {
+            //print("Stop");
+            if (!behaviourIsRunning)
+            {
+                //Debug.Log("NO BEHAVIPUR RUNING");
+                Stop();
+            }
+                
+        }
+    }
+
+    Vector3? GetClickedPoint()
+    {
+        /* Obtener punto seleccionado */
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+        int layer_mask = LayerMask.GetMask("Floor");
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layer_mask))
+        {
+            if (hit.collider.gameObject.name != "Piso")
+            {
+                Debug.Log("NO HAY NADA");
+                return null;
+            }
+            //Vector3 point  = transform.InverseTransformPoint(hit.point);
+            Debug.Log("PISO CLICKEADO");
+            return hit.point;
+        }
+        return null;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickedPoint = GetClickedPoint();
+        }
+        WASD();
+    }
+
+    public void TurnLeft(bool notify=false)
+    {
+        if (notify)
+        {
+            stopped = false;
+        }
+        Send_motors_pwm(-LVelocity, RVelocity);
+    }
+
+    public void TurnRight( bool notify=false )
+    {
+        if (notify)
+        {
+            stopped = false;
+        }
+        
+        Send_motors_pwm(LVelocity, -RVelocity);
+    }
+
+    public void GoForward(bool notify=false)
+    {
+        if (notify)
+        {
+            stopped = false;
+        }
+        
+        Send_motors_pwm(RVelocity-7, RVelocity);
+    }
+
+    public void GoBackwards(bool notify = false)
+    {
+        if (notify)
+        {
+            stopped = false;
+        }
+        Send_motors_pwm(-LVelocity, -LVelocity+2);
+    }
+
+    public void Stop(bool notify=false)
+    {   
+        if (notify)
+        {
+            stopped = true;
+        }
+
+        Send_motors_pwm();
+        backwards = forward = turningLeft = turningRight = false;
+    }
+
+    public void Send_motors_pwm(float left = 0, float right = 0)
+    {
+        int leftPWM = PWMHelper.Remap(left, -100, 100, pwmLBackward, pwmLForward);
+        int rightPWM = PWMHelper.Remap(right, -100, 100, pwmRBackward, pwmRForward);
+
+        StandardString msg = new StandardString
+        {
+            data = leftPWM.ToString() + "," + rightPWM.ToString()
+        };
+        robot.rosSocket.Publish(robot.movementPublisherId, msg);
+    }
+}
