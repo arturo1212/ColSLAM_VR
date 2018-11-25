caa8b926880b48ce30740ca69166b5b1be28d6b5
David A. Hernandez V <davidhervil@hotmail.com>
2018-11-25 11:58:19 -0400

    Bad syntax

--- Raspberry/tesis/arduino_reader.py
+++ Raspberry/tesis/arduino_reader.py
@@ -5,8 +5,8 @@ import sys
 import time
 ser = serial.Serial('/dev/ttyACM0', 115200)
 
-def callback(data)
-	msg = data.data
+def callback(data):
+    msg = data.data
     rospy.loginfo(rospy.get_caller_id() + "I claw %s", msg)
     action = int(msg)    
     ser.write(msg+"\n")
@@ -18,9 +18,9 @@ start = time.time()
 rospy.init_node('mirameste')
 rospy.Subscriber("claw", String, callback)
 while not rospy.is_shutdown():
-        result = str(ser.readline().strip())    # read up to one hundred bytes
-        #splitted = result.decode('utf-8').split(',')
-        #sys.stderr.write(splitted[0]+"\n")
-        #end = time.time()
-        pub.publish(result)
-        rate.sleep()
+    result = str(ser.readline().strip())    # read up to one hundred bytes
+    #splitted = result.decode('utf-8').split(',')
+    #sys.stderr.write(splitted[0]+"\n")
+    #end = time.time()
+    pub.publish(result)
+    rate.sleep()
